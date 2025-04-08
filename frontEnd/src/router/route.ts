692a3ca8647579a42af164e86e4529bfaf6529fd
SkyMazter <comunidadoscar@gmail.com>
2025-04-08 16:59:44 -0400

    deleted unnessecary files

--- frontEnd/src/router/route.ts
+++
@@ -1,6 +0,0 @@
-// import RouteConfig from "@react-router/dev/routes";
-
-// export default [
-//   index("./Components/PostView.tsx"),
-//   route("cat/:category", "./Components/FilteredView.tsx"),
-// ] satisfies RouteConfig;
