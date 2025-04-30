33e6916190b258dbff302279b58456a3cf6749d0
SkyMazter <comunidadoscar@gmail.com>
2025-04-30 17:41:00 -0400

    fixed connection.ts

--- server/src/connection.ts
+++ server/src/connection.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import "dotenv/config";
 
 const sequelize = new Sequelize({
-  host: "localhost",
+  host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
